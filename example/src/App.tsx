2121d2dfa8b7cdc7e2d757bb54790143831cfc82
Fardeen Panjwani <fardeen.panjwani@ucmouli.com>
2020-08-18 16:21:50 +0530

    added add-row functionality

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,64 @@
-import React from 'react'
+import * as React from 'react'
+import DynamicTable from 'react-dynamic-table'
 
-import { ExampleComponent } from 'react-dynamic-table'
-import 'react-dynamic-table/dist/index.css'
+const columns: any[] = [
+  {
+    name: 'From',
+    type: 'date'
+  },
+  {
+    name: 'End Date',
+    type: 'date'
+  },
+  {
+    name: 'Asset Operator',
+    type: 'text'
+  }
+]
+const rows: any[] = []
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+type Props = any
+type State = {
+  loading: boolean
+  rows: any[]
+  columns: string[]
 }
+export default class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
 
-export default App
+    this.state = {
+      loading: false,
+      rows: [],
+      columns: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      rows,
+      columns
+    })
+  }
+
+  onRowAdded = (e: React.MouseEvent<HTMLButtonElement>, row: any): any => {
+    e.preventDefault()
+
+    this.setState({
+      rows: [...this.state.rows, row]
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ width: '75vw', border: 'solid 1px #000', padding: '1rem' }}>
+        <DynamicTable
+          columns={this.state.columns}
+          rows={this.state.rows}
+          onRowAdded={this.onRowAdded}
+          title={'Operation History'}
+        />
+      </div>
+    )
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,212 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import {
+  Typography,
+  IconButton,
+  Paper,
+  TableContainer,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  TextField,
+  Button,
+  WithStyles,
+  withStyles,
+  TableFooter
+} from '@material-ui/core'
+import DeleteIcon from '@material-ui/icons/Delete'
 
-interface Props {
-  text: string
+const styles = () => ({
+  tableHead: {
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    color: '#000'
+  },
+  tableFooter: {
+    border: '1px solid grey'
+  },
+  buttonContainer: {
+    width: '100%',
+    background: 'gree',
+    minWidth: '250px',
+    justifyContent: 'flex-end'
+  }
+})
+
+type Props = {
+  title: string
+  columns: any[]
+  rows: any[]
+  onRowAdded: (e: React.MouseEvent<HTMLButtonElement>, row: any) => any
+} & WithStyles<typeof styles>
+interface State {
+  loading: boolean
+  rows: any[]
+  form: {
+    [key: string]: any
+  }
+}
+class DynamicTable extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+
+    this.state = {
+      loading: false,
+      rows: [],
+      form: {}
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      rows: this.props.rows
+    })
+  }
+
+  onRowAdded = (e: React.MouseEvent<HTMLButtonElement>): void => {
+    e.preventDefault()
+
+    const row: any = this.state.form
+    this.setState({
+      rows: [...this.state.rows, row]
+    })
+
+    this.props.onRowAdded(e, row)
+  }
+
+  onInput = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    console.log('key: ', e.target.name)
+    console.log('value: ', e.target.value)
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Typography variant={'h5'} gutterBottom>
+          {this.props.title}
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell className={this.props.classes.tableHead}>
+                  S No.
+                </TableCell>
+                {this.props.columns.map((column: any) => (
+                  <TableCell className={this.props.classes.tableHead}>
+                    {column.name}
+                  </TableCell>
+                ))}
+                <TableCell className={this.props.classes.tableHead}>
+                  Actions
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {this.props.rows.map((row: any, index: number) => (
+                <TableRow>
+                  <TableCell>{index + 1}</TableCell>
+                  {this.props.columns.map((column: any) => {
+                    const key: string = column.name
+                      .toLowerCase()
+                      .replace(/ /g, '_')
+                    return <TableCell>{row[key]}</TableCell>
+                  })}
+                  <TableCell>
+                    <IconButton color={'secondary'}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell />
+                {this.props.columns.map((column: any) => (
+                  <TableCell>
+                    <TextField
+                      label={column.name}
+                      fullWidth
+                      InputLabelProps={
+                        column.type === 'date'
+                          ? {
+                              shrink: true
+                            }
+                          : {}
+                      }
+                      variant={'outlined'}
+                      margin={'dense'}
+                      size={'small'}
+                      type={column.type || 'text'}
+                      name={column.name.toLowerCase().replace(/ /g, '_')}
+                      onChange={this.onInput}
+                      placeholder={column.name}
+                    />
+                  </TableCell>
+                ))}
+                <TableCell>
+                  <Button
+                    fullWidth
+                    variant={'contained'}
+                    color={'primary'}
+                    onClick={this.onRowAdded}
+                  >
+                    Submit
+                  </Button>
+                </TableCell>
+                {/*
+<TableCell
+                  className={this.props.classes.tableFooter}
+                  colSpan={this.props.columns.length}
+                >
+                  <form noValidate autoComplete={'off'}>
+                    <Grid container spacing={2}>
+                      {this.props.columns.map((column: any) => (
+                        <Grid item xs={4}>
+                          <TextField
+                            label={column.name}
+                            fullWidth
+                            InputLabelProps={{
+                              shrink: column.type === 'date'
+                            }}
+                            variant={'outlined'}
+                            margin={'dense'}
+                            size={'small'}
+                            type={column.type || 'text'}
+                            name={column.name.toLowerCase().replace(/ /g, '_')}
+                            onChange={this.onInput}
+                            placeholder={column.name}
+                          />
+                        </Grid>
+                      ))}
+                      <Grid item xs={12}>
+                        <ButtonGroup
+                          className={this.props.classes.buttonContainer}
+                        >
+                          <Button color={'secondary'}>Reset</Button>
+                          <Button color={'primary'} onClick={this.onRowAdded}>
+                            Submit
+                          </Button>
+                        </ButtonGroup>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </TableCell>
+                */}
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </TableContainer>
+      </React.Fragment>
+    )
+  }
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export default withStyles(styles)(DynamicTable)
